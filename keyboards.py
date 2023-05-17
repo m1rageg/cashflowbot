21816381f705f38ab9d7ab752ad95ddf38c27f96
m1rageg <egor.sergei.demin153@gmail.com>
2023-05-18 01:43:10 +0300

    add new buttons

--- keyboards.py
+++ keyboards.py
@@ -3,5 +3,15 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 currency = InlineKeyboardMarkup(row_width=2)
 currency.add(InlineKeyboardButton(text='UAH ₴', callback_data='uah'),
-             InlineKeyboardButton(text='USD $', callback_data='usd'),
-             InlineKeyboardButton(text='UAH ₴, USD $', callback_data='uah+usd'))
+             InlineKeyboardButton(text='USD $', callback_data='usd'))
+
+
+confirmuah = InlineKeyboardMarkup(row_width=1)
+confirmuah.add(InlineKeyboardButton(text='Так', callback_data='yes uah'),
+            InlineKeyboardButton(text='Ні, обрати ще раз', callback_data='no'))
+
+
+confirmusd = InlineKeyboardMarkup(row_width=1)
+confirmusd.add(InlineKeyboardButton(text='Так', callback_data='yes usd'),
+            InlineKeyboardButton(text='Ні, обрати ще раз', callback_data='no'))
+
