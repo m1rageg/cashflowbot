8a4e1dd53966db5ef5fb8bfef37c7ed4bb1635af
m1rageg <egor.sergei.demin153@gmail.com>
2023-05-19 02:46:17 +0300

    Update creating table and add new functions

--- db.py
+++ db.py
@@ -16,10 +16,37 @@ async def create_individual_table(chat_id, user_id):
     if not user:
         cur.execute("INSERT INTO wallet (tg_id) VALUES({key})".format(key=user_id))
         cur.execute(f"CREATE TABLE IF NOT EXISTS user_categories_{chat_id} ("
-                    "current_amount FLOAT,"
-                    "currency TEXT" 
-                    "Їжа FLOAT,"
-                    "Досуг FLOAT,"
-                    "Авто FLOAT,"
-                    "Комуналка FLOAT)")
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    "current_amount FLOAT, "
+                    "currency TEXT, "
+                    "Дохід FLOAT, "
+                    "Подорожі FLOAT, "
+                    "Освіта FLOAT, "
+                    "Їжа FLOAT, "
+                    "Досуг FLOAT, "
+                    "Авто FLOAT, "
+                    "Таксі FLOAT, "
+                    "`Комуналка(житло)` FLOAT, "
+                    "Інше FLOAT)"
+                    )
+        db.commit()
+
+
+async def inserting_currency(user_id, currency):
+    table_name = f"user_categories_{user_id}"
+    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+    existing_table = cur.fetchone()
+    if existing_table:
+        column_name = "currency"
+        cur.execute(f"INSERT INTO {table_name} ({column_name}) VALUES (?)", (currency,))
+        db.commit()
+
+
+async def inserting_sum_first_time(user_id, amount):
+    table_name = f"user_categories_{user_id}"
+    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+    existing_table = cur.fetchone()
+    if existing_table:
+        column_name = "current_amount"  # Имя целевой колонки
+        cur.execute(f"UPDATE {table_name} SET {column_name} = ? WHERE id = 1", (amount,))
         db.commit()
